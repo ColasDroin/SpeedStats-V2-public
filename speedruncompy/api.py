2f5c28aec59989a8cbe716a596b070bdcd9f4a6d
Colas Droin <colasdroin@gmail.com>
2025-01-08 14:41:35 +0100

    Update timeout to avoid rate limits

--- speedruncompy/api.py
+++ speedruncompy/api.py
@@ -1,20 +1,24 @@
-import base64, json
-from .exceptions import *
+import base64
+import json
 import logging
-from ReturnThread import ReturnThread
-from requests import Response, get, post, ReadTimeout
 from time import sleep
-from typing import Callable, Any
+from typing import Any, Callable
+
+from requests import ReadTimeout, Response, get, post
+
+from ReturnThread import ReturnThread
+
+from .exceptions import *
 
 API_URI = "https://www.speedrun.com/api/v2/"
 API_V1_URI = "https://www.speedrun.com/api/v1/"
 LANG = "en"
 ACCEPT = "application/json"
 USE_PROXY = False
-TIMEOUT = 60
-MAX_ATTEMPTS = 10
+TIMEOUT = 80
+MAX_ATTEMPTS = 15
 
-PROXIES = [] # If you set up proxies on Heroku, put their URLs here
+PROXIES = []  # If you set up proxies on Heroku, put their URLs here
 
 cookie = {}
 proxyNum = -1
@@ -24,12 +28,13 @@ usableProxies = []
 _log = logging.getLogger("speedruncompy")
 _main_log = logging.getLogger("SpeedStats-V2")
 
+
 def findUsableProxies():
     _main_log.info("Finding usable proxies:")
-    
+
     proxyIPThreads = {}
     for proxy in PROXIES:
-        t = ReturnThread(target=getIP, args = (proxy, ))
+        t = ReturnThread(target=getIP, args=(proxy,))
         proxyIPThreads[proxy] = t
         t.start()
 
@@ -41,30 +46,33 @@ def findUsableProxies():
             usableProxies.append(proxy)
         else:
             _main_log.info(f"IP of {proxy} is {ip}, which already being used. Skipping.")
-    
+
     _main_log.info(f"Found {len(usableProxies)} proxies.")
     return len(usableProxies)
 
+
 def getProxyUri():
-    if (not USE_PROXY):
-       return ""
-    
+    if not USE_PROXY:
+        return ""
+
     if len(usableProxies) == 0:
         findUsableProxies()
-    
+
     global proxyNum
     proxyNum = (proxyNum + 1) % len(usableProxies)
     return usableProxies[proxyNum]
+
 
 def setSessId(phpsessionid):
     global cookie
     cookie.update({"PHPSESSID": phpsessionid})
 
+
 def getIP(proxy: str):
     _header = {"Accept-Language": LANG, "Accept": ACCEPT}
 
     response = get(url=f"{proxy}ip4only.me/api/", headers=_header, timeout=TIMEOUT)
-    return response.content.decode('utf-8').split(',')[1]
+    return response.content.decode("utf-8").split(",")[1]
 
 
 def doGet(endpoint: str, params: dict = {}):
@@ -78,27 +86,38 @@ def doGet(endpoint: str, params: dict = {}):
     attempt = 0
     while attempt < MAX_ATTEMPTS:
         try:
-            response = get(url=f"{getProxyUri()}{API_URI}{endpoint}", headers=_header, params={"_r": _r}, timeout=TIMEOUT)
+            response = get(
+                url=f"{getProxyUri()}{API_URI}{endpoint}",
+                headers=_header,
+                params={"_r": _r},
+                timeout=TIMEOUT,
+            )
             return response
         except Exception:
             print(f"Attempt {attempt + 1} of {MAX_ATTEMPTS} failed due to timeout. Retrying...")
             attempt += 1
+
 
 def doGetV1(endpoint: str, params: dict = {}):
     _header = {"Accept-Language": LANG, "Accept": ACCEPT}
     paramsjson = bytes(json.dumps(params, separators=(",", ":")).strip(), "utf-8")
     _log.debug(f"GET {API_V1_URI}{endpoint} w/ params {paramsjson}")
-    
+
     attempt = 0
     while attempt < MAX_ATTEMPTS:
         try:
-            response = get(url=f"{getProxyUri()}{API_V1_URI}{endpoint}{buildParams(params)}", headers=_header, timeout=TIMEOUT)
+            response = get(
+                url=f"{getProxyUri()}{API_V1_URI}{endpoint}{buildParams(params)}",
+                headers=_header,
+                timeout=TIMEOUT,
+            )
             return response
         except Exception:
             print(f"Attempt {attempt + 1} of {MAX_ATTEMPTS} failed due to timeout. Retrying...")
             attempt += 1
 
-def doPost(endpoint:str, params: dict = {}, _setCookie=True):
+
+def doPost(endpoint: str, params: dict = {}, _setCookie=True):
     global cookie
     _header = {"Accept-Language": LANG, "Accept": ACCEPT}
     _log.debug(f"POST {API_URI}{endpoint} w/ params {params}")
@@ -106,7 +125,13 @@ def doPost(endpoint:str, params: dict = {}, _setCookie=True):
     attempt = 0
     while attempt < MAX_ATTEMPTS:
         try:
-            response = post(url=f"{getProxyUri()}{API_URI}{endpoint}", headers=_header, cookies=cookie, json=params, timeout=TIMEOUT)
+            response = post(
+                url=f"{getProxyUri()}{API_URI}{endpoint}",
+                headers=_header,
+                cookies=cookie,
+                json=params,
+                timeout=TIMEOUT,
+            )
             return response
         except Exception:
             print(f"Attempt {attempt + 1} of {MAX_ATTEMPTS} failed due to timeout. Retrying...")
@@ -116,18 +141,20 @@ def doPost(endpoint:str, params: dict = {}, _setCookie=True):
         cookie = response.cookies
     return response
 
+
 def buildParams(params):
     output = "?"
     for key, value in params.items():
         output += "{}={}&".format(key, value)
     return output
 
-class BaseRequest():
+
+class BaseRequest:
     def __init__(self, method: Callable[[str, dict[str, Any]], Response], endpoint, **params):
         self.method = method
         self.endpoint = endpoint
         self.params = params
-    
+
     def updateParams(self, **kwargs):
         """Updates parameters using values set in kwargs"""
         self.params.update(kwargs)
@@ -136,30 +163,54 @@ class BaseRequest():
         try:
             self.response = self.method(self.endpoint, self.params)
 
-            if (self.response.status_code >= 500 and self.response.status_code <= 599) or self.response.status_code == 408 or self.response.status_code == 429:
+            if (
+                (self.response.status_code >= 500 and self.response.status_code <= 599)
+                or self.response.status_code == 408
+                or self.response.status_code == 429
+            ):
                 if retries > 0:
-                    _log.error(f"SRC returned error {self.response.status_code} {self.response.content}. Retrying with delay {delay}:")
-                    for attempt in range(1, retries+1):
+                    _log.error(
+                        f"SRC returned error {self.response.status_code} {self.response.content}. Retrying with delay {delay}:"
+                    )
+                    for attempt in range(1, retries + 1):
                         self.response = self.method(self.endpoint, self.params)
-                        if not ((self.response.status_code >= 500 and self.response.status_code <= 599) or self.response.status_code == 408 or self.response.status_code == 429): 
+                        if not (
+                            (self.response.status_code >= 500 and self.response.status_code <= 599)
+                            or self.response.status_code == 408
+                            or self.response.status_code == 429
+                        ):
                             break
-                        _log.error(f"Retry {attempt} returned error {self.response.status_code} {self.response.content}")
+                        _log.error(
+                            f"Retry {attempt} returned error {self.response.status_code} {self.response.content}"
+                        )
                         sleep(delay)
                     else:
-                        if self.response.status_code == 408: raise RequestTimeout(self)
-                        elif self.response.status_code == 429: raise RateLimitExceeded(self)
-                        else: raise ServerException(self)
+                        if self.response.status_code == 408:
+                            raise RequestTimeout(self)
+                        elif self.response.status_code == 429:
+                            raise RateLimitExceeded(self)
+                        else:
+                            raise ServerException(self)
 
-            if self.response.status_code == 400: raise BadRequest(self)
-            if self.response.status_code == 401: raise Unauthorized(self)
-            if self.response.status_code == 403: raise Forbidden(self)
-            if self.response.status_code == 404: raise NotFound(self)
-            if self.response.status_code == 405: raise MethodNotAllowed(self)
-            if self.response.status_code == 408: raise RequestTimeout(self)
-            if self.response.status_code == 429: raise RateLimitExceeded(self)
+            if self.response.status_code == 400:
+                raise BadRequest(self)
+            if self.response.status_code == 401:
+                raise Unauthorized(self)
+            if self.response.status_code == 403:
+                raise Forbidden(self)
+            if self.response.status_code == 404:
+                raise NotFound(self)
+            if self.response.status_code == 405:
+                raise MethodNotAllowed(self)
+            if self.response.status_code == 408:
+                raise RequestTimeout(self)
+            if self.response.status_code == 429:
+                raise RateLimitExceeded(self)
 
             if self.response.status_code < 200 or self.response.status_code > 299:
-                _log.error(f"Unknown response error returned from SRC! {self.response.status_code} {self.response.content}")
+                _log.error(
+                    f"Unknown response error returned from SRC! {self.response.status_code} {self.response.content}"
+                )
                 raise APIException(self)
 
             return json.loads(self.response.content)
@@ -167,13 +218,16 @@ class BaseRequest():
             _log.error(f"ServerException caught: {e}")
             return None
 
+
 class GetRequest(BaseRequest):
     def __init__(self, endpoint, **params) -> None:
         super().__init__(method=doGet, endpoint=endpoint, **params)
 
+
 class GetRequestV1(BaseRequest):
     def __init__(self, endpoint, **params) -> None:
         super().__init__(method=doGetV1, endpoint=endpoint, **params)
+
 
 class PostRequest(BaseRequest):
     def __init__(self, endpoint, **params) -> None:
